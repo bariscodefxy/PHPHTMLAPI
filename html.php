42fbd159b97f364f02f20af7d35388dc83704533
bariscodefxy <bariscodefx@gmail.com>
2022-11-03 15:43:28 +0000

    Initial commit

---
+++ html.php
@@ -0,0 +1,26 @@
+<?php
+
+function h1($s="")
+{
+  return "<h1>$s</h1>";
+}
+
+function p($s="")
+{
+  return "<p>$s</p>";
+}
+
+function span($s="")
+{
+  return "<span>$s</span>";
+}
+
+function endl()
+{
+  echo br();
+}
+
+function br()
+{
+  return "<br>";
+}
